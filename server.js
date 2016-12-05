ec8bf8c5796106caee1d243f036a1b36ec8e9b1a
Nick Field <heyimnick@live.com>
2016-12-05 11:53:43 +0000

    add /welcome route for step 4

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const Hapi = require('hapi');
 const Inert = require('inert');
+const QueryString = require('querystring');
+const env = require('env2')('./config.env');
+const request = require('request');
 
 const server = new Hapi.Server();
 
@@ -11,13 +14,35 @@ server.connection({
 server.register(Inert, (err) => {
   if (err) throw err;
 
-  server.route({
+  server.route([{
     path: '/',
     method: 'GET',
     handler: (req, rep) => {
       rep.file('./index.html')
     }
-  })
+  },
+  {
+    path: '/login',
+    method: 'GET',
+    handler: (req, rep) => {
+      const baseURL = 'https://github.com/login/oauth/authorize'
+      const queries = QueryString.stringify({ client_id: process.env.CLIENT_ID, redirect_uri: 'http://localhost:8080/welcome'});
+      rep.redirect(`${baseURL}?${queries}`);
+    }
+  },
+  {
+    path: '/welcome',
+    method: 'GET',
+    handler: (req, rep) => {
+      const post = {
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+        code: req.query.code
+      }
+      request.post('https://github.com/login/oauth/access_token', form: post, function(err,httpResponse,body){ /* ... */ })
+    }
+  }
+])
 });
 
 server.start((err)=>{
