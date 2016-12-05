3059cdf63ae8d49af28b8f77cf939d25e2a1cb3a
Nick Field <heyimnick@live.com>
2016-12-05 14:30:05 +0000

    add eslintrc.js

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ const options = {
   ttl: 24 * 60 * 60 * 1000,
   isSecure: false,
   isHttpOnly: false
-}
+};
 
 const server = new Hapi.Server();
 
 server.connection({
   port: 8080,
   host: 'localhost'
-})
+});
 
 server.register([Inert, cookie], (err) => {
   if (err) throw err;
@@ -29,15 +29,15 @@ server.register([Inert, cookie], (err) => {
     path: '/',
     method: 'GET',
     handler: (req, rep) => {
-      rep.file('./index.html')
+      rep.file('./index.html');
     }
   },
   {
     path: '/login',
     method: 'GET',
     handler: (req, rep) => {
-      const baseURL = 'https://github.com/login/oauth/authorize'
-      const queries = QueryString.stringify({ client_id: process.env.CLIENT_ID, redirect_uri: 'http://localhost:8080/welcome'});
+      const baseURL = 'https://github.com/login/oauth/authorize';
+      const queries = QueryString.stringify({client_id: process.env.CLIENT_ID, redirect_uri: 'http://localhost:8080/welcome'});
       rep.redirect(`${baseURL}?${queries}`);
     }
   },
@@ -49,13 +49,14 @@ server.register([Inert, cookie], (err) => {
         client_id: process.env.CLIENT_ID,
         client_secret: process.env.CLIENT_SECRET,
         code: req.query.code
-      }
+      };
       request.post({url: 'https://github.com/login/oauth/access_token', form: post},
        (err, httpResponse, body) => {
+         if (err) throw err;
          const token = QueryString.parse(body).access_token;
          req.cookieAuth.set({access_token: token});
-          rep.redirect('/anything');
-      })
+         rep.redirect('/anything');
+       });
     }
   },
   {
@@ -68,7 +69,7 @@ server.register([Inert, cookie], (err) => {
       }
     },
     handler: (req, rep) => {
-      if(req.auth.isAuthenticated) {
+      if (req.auth.isAuthenticated) {
         request.get({
           url: 'https://api.github.com/user',
           headers: {
@@ -76,22 +77,21 @@ server.register([Inert, cookie], (err) => {
             Authorization: `token ${req.auth.credentials.access_token}`
           }
         }, (err, httpResponse, body) => {
-            if (err) console.log(err);
-            const parsed = JSON.parse(body)
-            console.log(parsed)
-            rep(`Hello ${parsed.name}, you live in ${(parsed.location || 'somewhere')},
+          if (err) console.log(err);
+          const parsed = JSON.parse(body);
+          rep(`Hello ${parsed.name}, you live in ${(parsed.location || 'somewhere')},
             <img src='${parsed.avatar_url}'/>
             `);
-          }
+        }
 
-        )
+        );
       }
     }
   }
-])
+  ]);
 });
 
-server.start((err)=>{
+server.start((err) => {
   if (err) throw err;
   console.log('Server started at port: 8080');
-})
+});
